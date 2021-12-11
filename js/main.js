d678a226713d43d9464ee2125789b3935dc2ce9b
sharsheev <Sharsheev@>
2021-12-11 15:19:53 +0300

    space_1
    part№3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+$('.slider__box') .slick({
+    prevArrow:  '<img class="slider__Arrow slider__arrow__left" src="images/Arrow 2.svg" alt="left"></img>',
+    nextArrow:  '<img class="slider__Arrow slider__arrow__right" src="images/Arrow 1.svg" alt="right"></img>',
+});
 });
